9dee1dddc6e673c8024f28cec3fc61d04cc87f7e
JoshOBrien <joshmobrien@gmail.com>
2018-02-16 18:27:45 -0800

    Improved gdal_translate example, using tempdir & side-by-side plots

--- R/gdal_translate.R
+++ R/gdal_translate.R
@@ -8,8 +8,34 @@
 ##' @examples
 ##' \dontrun{
 ##' ## Shrink a tiff by 50% in both x and y dimensions
-##' file.copy(system.file("extdata/europe.tif", package = "starsUtils"), ".")
-##' gdal_translate("europe.tif", "europe_small.tif", outsize=c("50%","50%"))
+##' td <- tempdir()
+##' in_raster <- file.path(td, "europe.tif")
+##' out_raster <- file.path(td, "europe_small.tif")
+##' file.copy(system.file("extdata/europe.tif", package = "starsUtils"), td)
+##' gdal_translate(in_raster, out_raster, outsize = c("50%","50%"))
+##'
+##' ## Check that it worked
+##' if(require(gridExtra) & require(raster) & require(rasterVis)) {
+##' r1 <- raster(in_raster)
+##' r1[is.na(r1)] <- 0
+##' r1 <- as.factor(r1)
+##' rat <- levels(r1)[[1]]
+##' rat[["landcover"]] <- c("water", "land")
+##' levels(r1) <- rat
+##' p1 <- levelplot(r1, margin=FALSE, colorkey=FALSE,
+##'                 col.regions = c("lightblue", "brown"))
+##'
+##' r2 <- raster(out_raster)
+##' r2[is.na(r2)] <- 0
+##' r2 <- as.factor(r2)
+##' rat <- levels(r2)[[1]]
+##' rat[["landcover"]] <- c("water", "land")
+##' levels(r2) <- rat
+##' p2 <- levelplot(r2, margin=FALSE, colorkey=FALSE,
+##'                 col.regions = c("lightblue", "brown"))
+##'
+##' grid.arrange(p1, p2, ncol=2)
+##' }
 ##' }
 gdal_translate <-
     function(src_dataset, dst_dataset, ..., ot, strict, of, b, mask,

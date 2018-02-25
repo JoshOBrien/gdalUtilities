2c8cdba1bbe015f792676190fd2b128e07e300e6
JoshOBrien <joshmobrien@gmail.com>
2018-02-25 14:54:03 -0800

    Dropped use of narg argument by process_repeated_flag function
    It was always going to be problematic, especially given the existence
    of repeatable arguments of variable length. (`gdal_translate`, for
    instance, takes a `-gcp` argument, typically repeated, which can have
    length 4 or 5.) There seems, on the other hand, to be no real downside
    to asking that users supply repeated arguments in the form of matrices
    in which each row represents an independent supplied argument.

--- R/process_args.R
+++ R/process_args.R
@@ -1,15 +1,15 @@
 ## Helper function used in process_args()
-process_repeated_flag <- function(flag, val, narg) {
+process_repeated_flag <- function(flag, val) {
     if(is.matrix(val)) {
         m <- t(val)
     } else {
-        m <- matrix(val, nrow=narg)
+        m <- matrix(val, ncol = 1)
     }
     as.vector(rbind(flag, m))
 }
 ## ## Example of a repeated flag for with several arguments for each flag:
 ## ## gdal_translate() can take 2D ground control points given by 4 args each
-## process_repeated_flag("-gcp", 1:12, 4)
+## process_repeated_flag("-gcp", 1:4)
 ## ## 3D ground control points take 5 args each; in R, specify using matrix
 ## process_repeated_flag("-gcp", matrix(1:20, ncol=5, byrow=TRUE))
 
@@ -17,13 +17,13 @@ process_repeated_flag <- function(flag, val, narg) {
 process_args <- function(args, formalsTable) {
     ft <- formalsTable
     opts <- lapply(names(args), function(nm) {
-        flag       <- ft[ft$arg==nm, "flag"]
-        narg       <- ft[ft$arg==nm, "narg"]
-        repeatable <- ft[ft$arg==nm, "repeatable"]
+        flag       <- ft[ft$arg == nm, "flag"]
+        narg       <- ft[ft$arg == nm, "narg"]
+        repeatable <- ft[ft$arg == nm, "repeatable"]
         val <- args[[nm]]
         if(repeatable) {
             ## Handle repeatable flags
-            process_repeated_flag(flag, val=val, narg=narg)
+            process_repeated_flag(flag, val = val)
         } else {
             ## Handle non-repeatable flags
             if(narg == 0) {

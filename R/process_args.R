116442ba96df9f7e4c6c47a7d9ed2cc5372a48c2
JoshOBrien <joshmobrien@gmail.com>
2019-07-28 18:39:15 -0700

    No-surprise handling of repeatable arguments with one arg per flag
    This addresses the issue raised by John Baumgartner in
    https://github.com/JoshOBrien/gdalUtilities/issues/2. He there notes
    that in a vector of multiple creation options (e.g. `co=c('TILED=YES',
    'COMPRESS=LZW')`), only the first option/vector element was properly
    passed along.
    
    Although we could insist in cases like this that multiple arguments
    ought be passed in as a one column matrix (i.e. `co =
    matrix(c('TILED=YES', 'COMPRESS=LZW'), ncol = 1)`, that would be
    obnoxiously pedantic. Instead, I just added a branch that allows users
    to just pass in vectors of arguments for repeatable flags that take
    one argument per flag.

--- R/process_args.R
+++ R/process_args.R
@@ -1,17 +1,26 @@
 ## Helper function used in process_args()
-process_repeated_flag <- function(flag, val) {
-    if(is.matrix(val)) {
+process_repeated_flag <- function(flag, val, narg) {
+    if (is.matrix(val)) {
         m <- t(val)
     } else {
-        m <- matrix(val, ncol = 1)
+        if (narg == 1) {
+            ## Properly handle vectors of multiple config options
+            ## (e.g. -oo, -co, -mo), which users are unlikely to pass
+            ## in as 1-column matrices
+            m <- matrix(val, nrow = 1)
+        } else {
+            ## For flags taking more than one argument, assume a
+            ## vector input means the argument isn't being repeated
+            m <- matrix(val, ncol = 1)
+        }
     }
     as.vector(rbind(flag, m))
 }
 ## ## Example of a repeated flag for with several arguments for each flag:
 ## ## gdal_translate() can take 2D ground control points given by 4 args each
-## process_repeated_flag("-gcp", 1:4)
+## process_repeated_flag("-gcp", 1:4, 4)
 ## ## 3D ground control points take 5 args each; in R, specify using matrix
-## process_repeated_flag("-gcp", matrix(1:20, ncol=5, byrow=TRUE))
+## process_repeated_flag("-gcp", matrix(1:20, ncol=5, byrow=TRUE), 4)
 
 ## Workhorse function
 process_args <- function(args, formalsTable) {
@@ -21,14 +30,14 @@ process_args <- function(args, formalsTable) {
         narg       <- ft[ft$arg == nm, "narg"]
         repeatable <- ft[ft$arg == nm, "repeatable"]
         val <- args[[nm]]
-        if(repeatable) {
+        if (repeatable) {
             ## Handle repeatable flags
-            process_repeated_flag(flag, val = val)
+            process_repeated_flag(flag, val = val, narg = narg)
         } else {
             ## Handle non-repeatable flags
-            if(narg == 0) {
+            if (narg == 0) {
                 ## Handle Boolean on/off flags
-                if(val) flag else NULL
+                if (val) flag else NULL
             } else {
                 ## Handle flags that expect their own arguments
                 c(flag, val)
